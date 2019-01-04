4fab74439cf1ff086195a657ce9ef59e44b0cb8d
yu.pan <yu.pan@hzjo.com>
2019-01-04 20:33:26 +0800

    finish server api comment module

---
+++ src/server/api/comment-controller/getComment.js
@@ -0,0 +1,18 @@
+import request from '@/server/utils/request'
+/**
+ * 获取评论
+ * @param commentId 评论id
+ * @param userId 用户id
+ * @param articleId 博文id
+ */
+export default (commentId, userId, articleId) => {
+  return request({
+    url: '/:comment/edit',
+    method: 'post',
+    params: {
+      user_id: userId,
+      comment_id: commentId,
+      article_id: articleId
+    }
+  })
+}
