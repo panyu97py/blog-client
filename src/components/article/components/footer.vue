61ee4bd14b42b23e3e414a05405f53fbc6bc4eed
yu.pan <panyupy@vip.qq.com>
2019-01-06 00:24:09 +0800

    add show all button and modify styles

--- src/components/article/components/footer.vue
+++ src/components/article/components/footer.vue
@@ -3,7 +3,12 @@
     <svg-icon icon-class="tag" class="article_tag_icon"/>
     <articleTagList>
       <articleTag tagName="标签1"/>
+      <articleTag tagName="标签1"/>
+      <articleTag tagName="标签1"/>
+      <articleTag tagName="标签1"/>
+      <articleTag tagName="标签1"/>
     </articleTagList>
+    <p class="article_show_all">展开全文 > ></p>
     <div style="clear:both"/>
   </div>
 </template>
--- src/components/tagList/item.vue
+++ src/components/tagList/item.vue
@@ -1,11 +1,22 @@
 <template>
-  <li class="tag_list_item">
+  <li class="tag_list_item" :class="theme">
    <a href class="tag">{{tagName}}</a>
   </li>
 </template>
 <script>
 export default {
   name: 'tag',
+  data () {
+    return {
+      classList: ['yellow', 'blue', 'pink', 'lightBlue', 'orange', 'green']
+    }
+  },
+  computed: {
+    theme () {
+      let index = Math.floor(Math.random() * this.classList.length)
+      return this.classList[index]
+    }
+  },
   props: {
     tagName: {
       type: String,
