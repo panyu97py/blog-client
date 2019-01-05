b461c33384e6eaaa96a1bb6964a6e5bcd95894d7
yu.pan <panyupy@vip.qq.com>
2019-01-05 17:57:40 +0800

    add article tag

--- src/components/article/components/footer.vue
+++ src/components/article/components/footer.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="article_footer">
     <svg-icon icon-class="tag" class="article_tag_icon"/>
+    <ul class="article_tag_list">
+      <li class="article_tag_list_item">
+        <articleTag/>
+      </li>
+    </ul>
+    <div style="clear:both"/>
+    <!-- <div><a href="" class="article_tag">标签</a></div> -->
   </div>
 </template>
-<style lang="scss">
-#article_footer {
-  .article_tag_icon {
-    color: #999999;
+<script>
+import articleTag from './tag'
+export default {
+  name: 'article_footer',
+  components: {
+    articleTag
   }
 }
-</style>
+</script>
---
+++ src/components/article/components/tag.vue
@@ -0,0 +1,3 @@
+<template>
+    <a href class="article_tag">标签</a>
+</template>
