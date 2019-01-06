b055bfb55c1760c420c487c4913d998807d8cb76
yu.pan <panyupy@vip.qq.com>
2019-01-06 19:38:16 +0800

    modify css

--- src/components/Layout/components/Navigation/Navigation_select.vue
+++ src/components/Layout/components/Navigation/Navigation_select.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="Navigation_select">
     <div class="switch_icon" @mouseenter="SelectStatus_1=true" @mouseleave="closeOption">
-      <img src="/static/img/Layout/Navigation/首页.png" alt>
+       <svg-icon icon-class="menu" class="switch_icon_svg" v-if="selectedOption==='menu'"/>
+       <svg-icon icon-class="tag" class="switch_icon_svg" v-if="selectedOption==='tag'"/>
+    </div>
       <div class="selectOption" @mouseenter="SelectStatus_2=true" v-if="SelectStatus_1||SelectStatus_2" @mouseleave="SelectStatus_2=false">
         <p @click="selectedOption='menu'">菜单</p>
         <p @click="selectedOption='tag'">标签</p>
       </div>
-    </div>
-
     <!-- v-if="SelectStatus" -->
   </div>
 </template>
