1413a6cb94c394c06a4e178f1bbc51133b9f5a21
yu.pan <yu.pan@hzjo.com>
2019-01-07 17:55:27 +0800

    modify css

--- src/App.vue
+++ src/App.vue
@@ -14,18 +14,3 @@ export default {
   }
 }
 </script>
-
-<style>
-@media (min-width: 768px) {
-  #app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-  }
-}
-</style>
