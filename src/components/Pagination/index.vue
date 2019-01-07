e7dd50c5aae3cb35e08078333c87b2e071e705c2
yu.pan <yu.pan@hzjo.com>
2019-01-07 14:29:53 +0800

    modify Pagination

--- src/components/Pagination/index.vue
+++ src/components/Pagination/index.vue
@@ -2,12 +2,12 @@
 <div id="Pagination">
   <div v-if="currentPage>(pagerCount-2)">
     <prev @click="currentPage-=1"/>
-    <PaginationItem :page="page + x" :currentPage='currentPage' v-for="page in pagerCount-2" :key="page" />
+    <PaginationItem @click="currentPage=page + x" :page="page + x" :currentPage='currentPage' v-for="page in pagerCount-2" :key="page" />
     <next  @click="currentPage+=1"/>
   </div>
   <div v-else>
     <prev @click="currentPage-=1"/>
-    <PaginationItem :page="page" :currentPage='currentPage' v-for="page in totalPage" :key="page"/>
+    <PaginationItem  @click="currentPage=page" :page="page" :currentPage='currentPage' v-for="page in totalPage" :key="page"/>
     <next  @click="currentPage+=1"/>
     <div style="clear:both"/>
   </div>
