f5b90f8815727ca99e6b09971def503aedb50df3
yu.pan <yu.pan@hzjo.com>
2019-01-08 21:03:10 +0800

    add icon hover

--- src/components/Layout/components/Navigation/Navigation_menu.vue
+++ src/components/Layout/components/Navigation/Navigation_menu.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
-    <p>博客主页</p>
-    <p>随笔</p>
-    <p>相册</p>
-    <p>个人主页</p>
-    <p>个人简历</p>
+    <div>
+      <p>博客主页</p>
+      <p>随笔</p>
+      <p>相册</p>
+      <p>个人主页</p>
+      <p>个人简历</p>
+    </div>
+    <div class="Navigation_icon_list">
+      <a href="https://github.com/panyu97py" class="Navigation_github_icon">
+        <svg-icon icon-class="github" class-name/>
+      </a>
+    </div>
   </div>
 </template>
+<script>
+export default {
+}
+</script>
--- src/components/Layout/components/Navigation/index.vue
+++ src/components/Layout/components/Navigation/index.vue
@@ -8,14 +8,6 @@
       <NavigationSelect v-model="model"/>
       <NavigationMenu v-if="model==='menu'"/>
       <NavigationTagList v-else-if="model==='tag'"/>
-      <div class="Navigation_icon_list">
-        <a href="https://github.com/panyu97py" class="Navigation_github_icon">
-          <svg-icon icon-class="github" class-name/>
-        </a>
-        <a class="Navigation_email_icon">
-          <svg-icon icon-class="email" class-name/>
-        </a>
-      </div>
     </div>
   </div>
 </template>
