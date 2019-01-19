6a0530b12b9566f6cad7036d96252093936aa771
yu.pan <panyupy@vip.qq.com>
2019-01-19 23:06:24 +0800

    add create articles

--- src/store/modules/login/index.js
+++ src/store/modules/login/index.js
@@ -2,26 +2,36 @@ import api from '@/server'
 const user = {
   state: {
     token: null,
-    loginStatus: false
+    userInfo: {},
+    loginStatus: true
   },
   getters: {
     token: state => state.token,
+    userInfo: state => state.userInfo,
     loginStatus: state => state.loginStatus
   },
   mutations: {
     SET_TOKEN: (state, token) => {
       state.token = token
     },
+    SET_USERINFO: (state, userInfo) => {
+      state.userInfo = userInfo
+    },
     CHANGE_LOGIN_STATUS: (state, loginStatus) => {
       state.loginStatus = loginStatus
     }
   },
   actions: {
-    async login ({commit}, {username, password}) {
+    async login ({commit, action}, {username, password}) {
       let res = await api.login(username, password)
       commit('SET_TOKEN', res)
       commit('CHANGE_LOGIN_STATUS', true)
+      action('gerUserInfo')
       return res
+    },
+    async gerUserInfo ({commit}) {
+      let userInfo = await api.getUserInfo()
+      commit('SET_USERINFO', userInfo)
     },
     loginOut ({commit}) {
       commit('SET_TOKEN', '')
--- src/views/manage/articleManage/createArticles/index.vue
+++ src/views/manage/articleManage/createArticles/index.vue
@@ -1,31 +1,44 @@
 <template>
-  <div>
+  <div id="createArticles">
     <div>
-      <tinymce :height="300" v-model="content"/>
+      <el-input v-model="form.articleTitle" placeholder="输入文章标题" class="createArticles_title"/>
+      <el-button type="primary" class="createArticles_save" @click="saveArticles">保存</el-button>
+      <tinymce :height="300" v-model="form.articleContent"/>
     </div>
-    <div class="editor-content" v-html="content"/>
   </div>
 </template>
 
 <script>
 import Tinymce from '@/components/Tinymce'
-
+import {mapGetters} from 'vuex'
 export default {
   name: 'TinymceDemo',
   components: { Tinymce },
+  computed: {
+    ...mapGetters(['userInfo']),
+    userId: () => {
+      return this.userInfo.user_id
+    }
+  },
   data () {
     return {
-      content:
-      `<h1 style="text-align: center;">Welcome to the TinyMCE demo!</h1><p style="text-align: center; font-size: 15px;"><img title="TinyMCE Logo" src="//www.tinymce.com/images/glyph-tinymce@2x.png" alt="TinyMCE Logo" width="110" height="97" /><ul>
+      form: {
+        articleContent: `<h1 style="text-align: center;">Welcome to the TinyMCE demo!</h1><p style="text-align: center; font-size: 15px;"><img title="TinyMCE Logo" src="//www.tinymce.com/images/glyph-tinymce@2x.png" alt="TinyMCE Logo" width="110" height="97" /><ul>
         <li>Our <a href="//www.tinymce.com/docs/">documentation</a> is a great resource for learning how to configure TinyMCE.</li><li>Have a specific question? Visit the <a href="https://community.tinymce.com/forum/">Community Forum</a>.</li><li>We also offer enterprise grade support as part of <a href="https://tinymce.com/pricing">TinyMCE premium subscriptions</a>.</li>
       </ul>`
+      }
+    }
+  },
+  methods: {
+    async saveArticles () {
+      let {articleTitle, articleContent} = this.form
+      await this.$api.addArticle(this.userId, articleTitle, articleContent)
+      this.$notify({
+        title: '成功',
+        message: '保存成功',
+        type: 'success'
+      })
     }
   }
 }
 </script>
-
-<style scoped>
-.editor-content{
-  margin-top: 20px;
-}
-</style>
