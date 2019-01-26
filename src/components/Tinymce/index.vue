1baffc721d6b383a105ab783596e6dd942394f5a
yu.pan <yu.pan@hzjo.com>
2019-01-26 14:13:41 +0800

    add code sample languages type

--- src/components/Tinymce/index.vue
+++ src/components/Tinymce/index.vue
@@ -89,6 +89,20 @@ export default {
       const _this = this
       window.tinymce.init({
         language: this.language,
+        codesample_languages: [
+          {text: 'HTML/XML', value: 'markup'},
+          {text: 'JavaScript', value: 'javascript'},
+          {text: 'CSS', value: 'css'},
+          {text: 'PHP', value: 'php'},
+          {text: 'Ruby', value: 'ruby'},
+          {text: 'Python', value: 'python'},
+          {text: 'Java', value: 'java'},
+          {text: 'C', value: 'c'},
+          {text: 'C#', value: 'csharp'},
+          {text: 'C++', value: 'cpp'},
+          {text: 'bash', value: 'bash'},
+          {text: 'yaml', value: 'yaml'}
+        ],
         selector: `#${this.tinymceId}`,
         height: this.height,
         body_class: 'panel-body ',
