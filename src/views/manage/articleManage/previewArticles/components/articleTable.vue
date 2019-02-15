e15322304aad15955e8c7cd453e2db153ccd4390
yu.pan <yu.pan@hzjo.com>
2019-02-15 17:13:03 +0800

    add serial

--- src/views/manage/articleManage/previewArticles/components/articleTable.vue
+++ src/views/manage/articleManage/previewArticles/components/articleTable.vue
@@ -1,6 +1,6 @@
 <template>
   <articleTable :data="data">
-    <el-table-column type="index" align="center"></el-table-column>
+    <el-table-column prop="article_serial_number" label="#" align="center"></el-table-column>
     <el-table-column prop="article_title" label="标题" align="center"></el-table-column>
     <el-table-column label="作者" align="center">
       <template slot-scope="scope">
