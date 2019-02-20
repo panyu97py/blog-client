3a26d58a59c20c3833ffae28f0cfe99603f1fc37
yu.pan <yu.pan@hzjo.com>
2019-02-20 10:55:21 +0800

    modify css

--- src/components/Layout/manage/components/Manage-main/index.vue
+++ src/components/Layout/manage/components/Manage-main/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="manage_main">
-    <transition name="fade" mode="out-in">
-      <router-view/>
-    </transition>
+    <div class="main_view">
+      <transition name="fade" mode="out-in">
+        <router-view/>
+      </transition>
+    </div>
   </div>
 </template>
